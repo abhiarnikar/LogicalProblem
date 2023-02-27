2eb3dd55afb129504d38adf971416d8c7b4fc4e4
abhiarnikar <abhiarnikar725@gmail.com>
2023-02-27 20:45:16 +0530

    Test

---
+++ InterviewQuestions/CodeStepbyStep.js
@@ -0,0 +1,102 @@
+// let a = []
+// let b = []
+// console.log(a==b)
+// console.log(a===b)
+
+// let a = []
+// let b = a
+// console.log(a==b)
+// console.log(a===b)
+
+// let a = [1,2,3,4]
+
+// console.log(...a)
+
+console.log(typeof NaN)
+
+// let data = {name:'abhijit'}
+
+// console.log(delete data.name)
+// console.log(data)
+
+// console.log(delete data)
+
+// let info = {
+//     firstname:'abhijit',
+//     age:31,
+//     city:'Pune'
+// }
+
+// let {age} = info
+// console.log(age)
+
+// let info2 = {
+//   lastname:'arnikar',
+//   language:'marathi'
+// }
+
+// let data = {...info,...info2}
+// console.log(data)
+
+// console.log(Promise.resolve(6))
+
+//JSON.parse() => It converts or parses the JSON to javascript value
+
+// let name = 'abhijit'
+
+// function getname(){
+//     console.log(name)
+//     let name = 'krushna'
+// }
+// getname()
+// Ans=>Uncaught ReferenceError: Cannot access 'name' before initialization
+
+// let name = 'Abhijit Arnikar'
+
+// console.log(!typeof name === 'string')
+// console.log(!typeof name === String)
+
+// let data  = [11,22,33,44]
+
+// delete data[2]
+// console.log(data) // Ans [ 11, 22, <1 empty item>, 44 ]
+
+// console.log(data.length) // 4
+
+// let a = [1,2,3,4]
+// let b = [4,5,6]
+
+// let c = [...a,...b]
+// console.log(c) // [1, 2, 3, 4,4, 5, 6] it takes the duplicate value
+
+// let name = 'Abhijit'
+
+// setTimeout(()=>{
+//     console.log(name)
+// },0)
+
+// name = 'krushna'
+
+// for(let i=0;i<3;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1)
+// }
+
+// for(var i=0;i<3;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1)
+// }
+//"use strict"
+// var x = 10
+
+// function greet(){
+//     y = 100
+//     console.log(x)
+// }
+// greet()
+
+// console.log(y)
+
+
--- InterviewQuestions/Photon.js
+++ InterviewQuestions/Photon.js
@@ -4,3 +4,41 @@ function select(a){
 }
 select(7)
 
+
+function add(a,b){
+    return a+b
+}
+
+let aa = add(12,13)
+console.log(aa)
+console.log(typeof(aa))
+
+let city = "pune"
+
+switch (city) {
+    case "pune":
+        console.log("MH")
+    case "indore":
+        console.log("MP")
+    case "jaipur":
+        console.log("RJ")
+    default:
+        console.log("incorrect city name")
+}
+console.log('******************')
+// in above switch case it checks for every condition and gives the all output
+// so to overcome to above problem we use break function
+
+switch (city) {
+    case "pune":
+        console.log("MH")
+        break
+    case "indore":
+        console.log("MP")
+        break
+    case "jaipur":
+        console.log("RJ")
+        break
+    default:
+        console.log("incorrect city name")
+}
